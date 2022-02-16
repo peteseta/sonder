27e744c903f30a80d6e151ec4be157331fecb38a
PeteSeta <petesetabandhu@gmail.com>
2022-02-17 00:51:35 +0700

    new feed design, previous and next

--- components/Feed.js
+++ components/Feed.js
@@ -2,15 +2,59 @@ import { useState, useEffect } from "react";
 
 export default function Feed() {
   //TODO find a way to fetch only some of the screams - as the db gets bigger this will make it impossible to load all the screams
+
   // init thoughts - initialState is an empty array
   const [screams, setScreams] = useState([]);
-  var currentScreamBody = " ";
 
-  // picks a random scream each button click and shows in feed
-  function getRandomScream() {
-    const randomScream = screams[(Math.random() * screams.length) >> 0];
-    currentScreamBody = randomScream.body;
+  // init random scream picker thing
+  let currentScreamBody = "";
+  let currentScreamIndex = 0;
+
+  // fisher-yates array shuffle
+  function shuffle(array) {
+    let currentIndex = array.length,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  }
+
+  function updateScreamBody(index) {
+    currentScreamBody = screams[index].body;
     document.getElementById("thought").innerHTML = currentScreamBody;
+  }
+
+  function nextScream() {
+    // if currentScreamIndex is the last item in the array, set currentScreamIndex to 0
+    if (currentScreamIndex === screams.length - 1) {
+      currentScreamIndex = 0;
+      updateScreamBody(currentScreamIndex);
+    } else {
+      currentScreamIndex++;
+      updateScreamBody(currentScreamIndex);
+    }
+  }
+
+  function previousScream() {
+    if (currentScreamIndex === 0) {
+      currentScreamIndex = screams.length - 1;
+      updateScreamBody(currentScreamIndex);
+    } else {
+      currentScreamIndex--;
+      updateScreamBody(currentScreamIndex);
+    }
   }
 
   // on page load, API get request to get screams + sets a random scream for feed
@@ -19,13 +63,13 @@ export default function Feed() {
       "https://asia-southeast1-sonder-e7919.cloudfunctions.net/api/screams"
     );
     const screams = await response.json();
-    setScreams(screams);
+    setScreams(shuffle(screams));
 
-    // sets feed to view the latest thought, just once, for some reason it breaks if you call getRandomScream()
-    const randomScream = screams[(Math.random() * screams.length) >> 0];
-    currentScreamBody = randomScream.body;
+    // show random scream
+    currentScreamIndex = (Math.random() * screams.length) >> 0;
+    const firstLoadRandomScream = screams[currentScreamIndex];
+    currentScreamBody = firstLoadRandomScream.body;
     document.getElementById("thought").innerHTML = currentScreamBody;
-    document.getElementById("thought").classList.add = "hidden";
   };
 
   // run fetchScream on first render
@@ -34,25 +78,59 @@ export default function Feed() {
   }, []);
 
   return (
-    <div className="py-6 space-y-2 px-14">
+    <div className="flex flex-row space-x-4 px-14">
+      <button
+        id="btnPrevious"
+        onClick={previousScream}
+        className="basis-[5%] flex-auto rounded-lg p-4 border-2 border-neutral-900 text-neutral-100 opacity-75 hover:opacity-100 hover:scale-105 active:delay-0 active:duration-0 active:border-neutral-800 text-xl bold antialiased delay-100 ease-in-out duration-300 hover:shadow-[0_0px_25px_0px_rgba(255,255,255,0.2)]"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-5 h-5 stroke-neutral-100"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M10 19l-7-7m0 0l7-7m-7 7h18"
+          />
+        </svg>
+        prev
+      </button>
+
       <div
         id="thoughtBlock"
-        className="min-w-full min-h-full px-4 py-4 text-xl border-2 rounded-lg border-neutral-900 text-neutral-100"
+        className="flex-grow px-4 py-4 text-2xl border-2 rounded-lg basis-[90%] overscroll-contain border-neutral-900 text-neutral-100"
       >
-        <blockquote
+        <div
           id="thought"
-          className="break-words transition-all opacity-100 min-h-max overscroll-contain"
-        >
-          <p className="break-words"></p>
-        </blockquote>
+          className="break-words transition-all opacity-100 overscroll-contain"
+        ></div>
       </div>
 
       <button
-        id="btn-fade"
-        onClick={getRandomScream}
-        className="z-0 flex-none rounded-lg px-4 py-2 border-2 border-neutral-900 text-neutral-100 text-xl bold antialiased disabled:opacity-50 disabled:hover:scale-100 bg-transparent disabled:shadow-none delay-100 ease-in-out hover:scale-105 duration-300 active:underline hover:shadow-[0_0px_25px_0px_rgba(255,255,255,0.2)]"
+        id="btnNext"
+        onClick={nextScream}
+        className="basis-[5%] flex-auto rounded-lg p-4 border-2 border-neutral-900 text-neutral-100 opacity-75 hover:opacity-100 hover:scale-105 active:delay-0 active:duration-0 active:border-neutral-800 text-xl bold antialiased delay-100 ease-in-out duration-300 hover:shadow-[0_0px_25px_0px_rgba(255,255,255,0.2)]"
       >
-        read another
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-5 h-5 stroke-neutral-100"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M14 5l7 7m0 0l-7 7m7-7H3"
+          />
+        </svg>
+        next
       </button>
     </div>
   );
