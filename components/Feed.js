537dc23827f04fd23e729dbdf66d7166295bbe64
PeteSeta <petesetabandhu@gmail.com>
2022-02-16 22:34:41 +0700

    refactor and overflow fixed

--- components/Feed.js
+++ components/Feed.js
@@ -1,11 +1,17 @@
 import { useState, useEffect } from "react";
-import thoughts from "../pages/thoughts";
 
 export default function Feed() {
   //TODO find a way to fetch only some of the screams - as the db gets bigger this will make it impossible to load all the screams
   // init thoughts - initialState is an empty array
   const [screams, setScreams] = useState([]);
-  const currentScreamBody = "";
+  var currentScreamBody = " ";
+
+  // picks a random scream each button click and shows in feed
+  function getRandomScream() {
+    const randomScream = screams[(Math.random() * screams.length) >> 0];
+    currentScreamBody = randomScream.body;
+    document.getElementById("thought").innerHTML = currentScreamBody;
+  }
 
   // on page load, API get request to get screams + sets a random scream for feed
   const fetchScream = async () => {
@@ -15,25 +21,12 @@ export default function Feed() {
     const screams = await response.json();
     setScreams(screams);
 
-    // sets feed to view the latest thought, just once
+    // sets feed to view the latest thought, just once, for some reason it breaks if you call getRandomScream()
     const randomScream = screams[(Math.random() * screams.length) >> 0];
     currentScreamBody = randomScream.body;
     document.getElementById("thought").innerHTML = currentScreamBody;
-
-    //TODO remove on prod
-    console.log("screams fetched");
+    document.getElementById("thought").classList.add = "hidden";
   };
-
-  // picks a random scream each button click and shows in feed
-  function getRandomScream() {
-    const randomScream = screams[(Math.random() * screams.length) >> 0];
-    currentScreamBody = randomScream.body;
-    document.getElementById("thought").innerHTML = currentScreamBody;
-
-    //TODO remove on prod
-    console.log(randomScream);
-    console.log(currentScreamBody);
-  }
 
   // run fetchScream on first render
   useEffect(() => {
@@ -44,18 +37,22 @@ export default function Feed() {
     <div className="py-6 space-y-2 px-14">
       <div
         id="thoughtBlock"
-        className="relative px-4 py-4 text-xl border-2 rounded-lg opacity-100 min-h-fit max-w-screen overscroll-contain border-neutral-900 text-neutral-100"
+        className="min-w-full min-h-full px-4 py-4 text-xl border-2 rounded-lg border-neutral-900 text-neutral-100"
       >
-        <blockquote className="transition-all opacity-100 overscroll-contain">
-          <p id="thought"></p>
+        <blockquote
+          id="thought"
+          className="break-words transition-all opacity-100 min-h-max overscroll-contain"
+        >
+          <p className="break-words"></p>
         </blockquote>
       </div>
 
       <button
+        id="btn-fade"
         onClick={getRandomScream}
-        className="btn-fade z-0 flex-none rounded-lg px-4 py-2 border-2 border-neutral-900 text-neutral-100 text-xl bold antialiased disabled:opacity-50 disabled:hover:scale-100 bg-transparent disabled:shadow-none delay-100 ease-in-out hover:scale-105 duration-300 active:underline hover:shadow-[0_0px_25px_0px_rgba(255,255,255,0.2)]"
+        className="z-0 flex-none rounded-lg px-4 py-2 border-2 border-neutral-900 text-neutral-100 text-xl bold antialiased disabled:opacity-50 disabled:hover:scale-100 bg-transparent disabled:shadow-none delay-100 ease-in-out hover:scale-105 duration-300 active:underline hover:shadow-[0_0px_25px_0px_rgba(255,255,255,0.2)]"
       >
-        load screams
+        read another
       </button>
     </div>
   );
