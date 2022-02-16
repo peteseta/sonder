a6614fee518a888e3ad4cb2424da0f0e6b02e0ee
PeteSeta <petesetabandhu@gmail.com>
2022-02-16 16:19:34 +0700

    headwind ordering changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,29 +1,29 @@
 export default function Navbar() {
   return (
-    <nav className="bg-black px-14 pt-10 pb-16">
+    <nav className="pt-10 pb-16 bg-black px-14">
       <div
-        className="inline-flex flex-col space-y-12 items-start justify-start pr-14 pb-14"
+        className="inline-flex flex-col items-start justify-start space-y-12 pr-14 pb-14"
         style={{ width: 405, height: 86 }}
       >
-        <p className="w-2/3 h-10 text-8xl font-bold text-white antialiased">
+        <p className="w-2/3 h-10 antialiased font-bold text-white text-8xl">
           sonder.
         </p>
         <div
-          className="inline-flex space-x-4 items-start justify-start"
+          className="inline-flex items-start justify-start space-x-4"
           style={{ width: 405, height: 31 }}
         >
           <a href="/">
-            <button className="h-full text-4xl text-neutral-200 transition ease-in-out hover:scale-105 hover:text-white duration-300 antialiased">
+            <button className="h-full text-4xl antialiased transition duration-300 ease-in-out text-neutral-200 hover:scale-105 hover:text-white">
               write
             </button>
           </a>
           <a href="/thoughts">
-            <button className="h-full text-4xl text-neutral-200 transition ease-in-out hover:scale-105 hover:text-white duration-300 antialiased">
+            <button className="h-full text-4xl antialiased transition duration-300 ease-in-out text-neutral-200 hover:scale-105 hover:text-white">
               read
             </button>
           </a>
           <a href="/about">
-            <button className="h-full text-4xl text-neutral-200 transition ease-in-out hover:scale-105 hover:text-white duration-300 antialiased">
+            <button className="h-full text-4xl antialiased transition duration-300 ease-in-out text-neutral-200 hover:scale-105 hover:text-white">
               about
             </button>
           </a>
