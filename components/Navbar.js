e4429ac2cb907fa5ef0cf81753a8f18ebed9e9dd
PeteSeta <petesetabandhu@gmail.com>
2022-02-18 15:08:01 +0700

    using next/link instead for cleaner transitions

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,26 +1,49 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 export default function Navbar() {
+  const router = useRouter();
+
   return (
-    <nav className="px-6 pt-6 bg-black sm:px-14 sm:pt-10">
+    <nav className="px-6 pt-4 bg-black sm:px-14 sm:pt-10">
       <div className="inline-flex flex-col items-start justify-start pb-6 space-y-12 sm:pb-10">
         <p className="w-2/3 h-10 antialiased font-bold text-white text-8xl">
           sonder.
         </p>
         <div className="inline-flex items-start justify-start space-x-4">
-          <a href="/">
-            <button className="h-full text-4xl antialiased transition duration-300 ease-in-out text-neutral-200 hover:scale-105 hover:text-white">
+          <Link href="/">
+            <button
+              className={
+                router.pathname == "/"
+                  ? "h-full text-4xl underline decoration-[6px] decoration-neutral-900 underline-offset-[6px] antialiased transition duration-300 ease-in-out text-neutral-200 hover:scale-105 hover:text-white"
+                  : "h-full text-4xl no-underline decoration-neutral-900 antialiased transition duration-300 ease-in-out text-neutral-200 hover:scale-105 hover:text-white"
+              }
+            >
               write
             </button>
-          </a>
-          <a href="/thoughts">
-            <button className="h-full text-4xl antialiased transition duration-300 ease-in-out text-neutral-200 hover:scale-105 hover:text-white">
+          </Link>
+          <Link href="/thoughts">
+            <button
+              className={
+                router.pathname == "/thoughts"
+                  ? "h-full text-4xl underline decoration-[6px] decoration-neutral-900 underline-offset-[6px] antialiased transition duration-300 ease-in-out text-neutral-200 hover:scale-105 hover:text-white"
+                  : "h-full text-4xl no-underline decoration-neutral-900 antialiased transition duration-300 ease-in-out text-neutral-200 hover:scale-105 hover:text-white"
+              }
+            >
               read
             </button>
-          </a>
-          <a href="/about">
-            <button className="h-full text-4xl antialiased transition duration-300 ease-in-out text-neutral-200 hover:scale-105 hover:text-white">
+          </Link>
+          <Link href="/about">
+            <button
+              className={
+                router.pathname == "/about"
+                  ? "h-full text-4xl underline decoration-[6px] decoration-neutral-900 underline-offset-[6px] antialiased transition duration-300 ease-in-out text-neutral-200 hover:scale-105 hover:text-white"
+                  : "h-full text-4xl no-underline decoration-neutral-900 antialiased transition duration-300 ease-in-out text-neutral-200 hover:scale-105 hover:text-white"
+              }
+            >
               about
             </button>
-          </a>
+          </Link>
         </div>
       </div>
     </nav>
