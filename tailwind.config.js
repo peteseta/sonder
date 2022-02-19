047f465d5747277c4460e03f98068938febf9f20
PeteSeta <petesetabandhu@gmail.com>
2022-02-19 19:09:52 +0700

    component and dependancy cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,10 +16,6 @@ module.exports = {
       ],
     },
     extend: {
-      boxShadow: {
-        xl_white:
-          "0 20px 25px -5px rgba(255, 255, 255, 0.1), 0 10px 10px -5px rgba(255, 255, 255, 0.04)",
-      },
       animation: {
         blob: "blob 7s infinite",
       },
