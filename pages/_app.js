974cf212555b0b9eff7eef903f2b5d193d8c65d0
PeteSeta <petesetabandhu@gmail.com>
2022-02-19 22:07:23 +0700

    instagram window issue 2

--- pages/_app.js
+++ pages/_app.js
@@ -32,10 +32,23 @@ function MyApp({ Component, pageProps }) {
   //State of Screen width
   const [width, setWidth] = useState(window.innerWidth);
 
+  // Screen width pixels for tablets
+  const tabletBreakpoint = 767;
+
+  // When screen is resized
+  useEffect(() => {
+    const handleWindowResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleWindowResize);
+    // Return a function from the effect that removes the event listener
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, []);
+
   // Fix instagram window.innerwidth issue
   useEffect(() => {
-    if (window.screen.width < window.innerWidth) {
-      setSmall(true);
+    if (typeof window !== "undefined") {
+      if (window.screen.width < window.innerWidth) {
+        setWidth(window.screen.width);
+      }
     }
   }, []);
 
