ababc5c9a9b491fc0b1bbf443a9b05f99dfc735e
peteseta <petesetabandhu@gmail.com>
2023-02-07 13:12:08 +0700

    removed random feed sorting

--- components/Feed.js
+++ components/Feed.js
@@ -13,21 +13,21 @@ export default function Feed() {
 
   // fisher-yates array shuffle
   function shuffle(array) {
-    let currentIndex = array.length,
-      randomIndex;
+    // let currentIndex = array.length,
+    //   randomIndex;
 
-    // While there remain elements to shuffle...
-    while (currentIndex != 0) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
+    // // While there remain elements to shuffle...
+    // while (currentIndex != 0) {
+    //   // Pick a remaining element...
+    //   randomIndex = Math.floor(Math.random() * currentIndex);
+    //   currentIndex--;
 
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex],
-        array[currentIndex],
-      ];
-    }
+    //   // And swap it with the current element.
+    //   [array[currentIndex], array[randomIndex]] = [
+    //     array[randomIndex],
+    //     array[currentIndex],
+    //   ];
+    // }
 
     return array;
   }
